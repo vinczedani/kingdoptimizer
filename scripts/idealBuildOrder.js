7808d94ac2feaa3e1ba09425ddfc4503d43f9df3
Daniel Vincze <daniel.vincze@ibm.com>
2019-11-12 12:29:38 +0100

    factor in required building costs

--- scripts/idealBuildOrder.js
+++ scripts/idealBuildOrder.js
@@ -13,12 +13,50 @@ function getCode(building, offset = 0) {
   return `${building.name}:${building.level - offset}`;
 }
 
+function uniqueBuildings(buildings) {
+  const map = new Map();
+  const result = [];
+  for (const building of buildings) {
+    if(!map.has(getCode(building))) {
+      map.set(getCode(building), true);
+      result.push(building);
+    }
+  }
+  return result;
+}
+
 function findMissingRequirements(building) {
   const required = [...requirements[building.name]];
   if (building.level > 1) {
     required.push(getCode(building, 1));
   }
-  return required.filter((requiredBuilding) => !built.includes(requiredBuilding));
+  const missingCodes = required.filter((requiredBuilding) => !built.includes(requiredBuilding));
+  const missingBuildings = missingCodes.reduce((acc, missingBuildingCode) => {
+    const [buildingName, level] = missingBuildingCode.split(':');
+    const requiredBuilding = costData.find(buildingData => buildingData.name == buildingName && buildingData.level == level);
+    const missingReq = findMissingRequirements(requiredBuilding);
+
+    acc.push(requiredBuilding, ...missingReq);
+    return acc;
+  }, []);
+
+  return uniqueBuildings(missingBuildings);
+}
+
+function forceBuild(building) {
+  // building is already up as a requirement
+  if (built.includes(getCode(building))) {
+    return;
+  }
+  const missing = findMissingRequirements(building);
+
+  if (missing.length === 0) {
+    buildOrder.push(building);
+    built.push(getCode(building));
+  } else {
+    missing.map(forceBuild);
+    forceBuild(building);
+  }
 }
 
 function buildBuilding(building, isCapital) {
@@ -36,24 +74,40 @@ function buildBuilding(building, isCapital) {
     buildOrder.push(building);
     built.push(getCode(building));
   } else {
-    missing.map(missingBuildingCode => {
-      const [buildingName, level] = missingBuildingCode.split(':');
-      const requiredBuilding = costData.find(buildingData => buildingData.name == buildingName && buildingData.level == level);
-      buildBuilding(requiredBuilding);
-    });
-    buildBuilding(building);
+    if (!building.requirementsAdded) {
+      const requirementCostSum = missing.reduce((sum, required) => sum + required.levelCost, 0);
+      const requirementCPSum = missing.reduce((sum, required) => sum + required.cpGain, 0);
+      building.levelCostWithRequirements = building.levelCost + requirementCostSum;
+      building.cpGainWithRequirements = building.cpGain + requirementCPSum;
+      building.costPerCpGainWithRequirements = building.levelCostWithRequirements / building.cpGainWithRequirements;
+      building.requirementCount = missing.length;
+      building.requirementsAdded = true;
+    }
+
+    if (building.costPerCpGainWithRequirements < (costData[0].costPerCpGainWithRequirements || costData[0].costPerCpGain)) {
+      forceBuild(building);
+    } else {
+      costData.push(building);
+      costData.sort((a, b) => {
+        return (a.costPerCpGainWithRequirements || a.costPerCpGain) - (b.costPerCpGainWithRequirements ||b.costPerCpGain);
+      });
+    }
   }
 }
 
 function saveBuildOrder() {
   const buildOrderPretty = buildOrder.map(b => {
-    return `${b.name} lvl${b.level}: You gain ${b.cpGain} for ${b.levelCost} res. Res/cp: ${b.costPerCpGain}`;
+    let string = `${b.name} lvl${b.level}: You gain ${b.cpGain}cp for ${b.levelCost} res. Res/cp: ${b.costPerCpGain}\n`;
+    if (b.requirementsAdded) {
+      string += `It has ${b.requirementCount} required building levels (listed above).\nCombined cost: ${b.levelCostWithRequirements} adding ${b.cpGainWithRequirements} cp overall (avg: ${b.costPerCpGainWithRequirements})\n`;
+    }
+    return string;
   });
   fs.writeFileSync('buildOrderPretty', buildOrderPretty.join('\n'));
   fs.writeFileSync('buildOrder.json', JSON.stringify(buildOrder, null, 2));
 }
 
-function computeIdealBuildOrder(isCapital) {
+async function computeIdealBuildOrder(isCapital) {
   while (costData.length > 0) {
     const nextBestBuilding = costData.shift();
     buildBuilding(nextBestBuilding, isCapital);
