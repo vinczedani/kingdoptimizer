4fe6446deb7f2e8cbc30d666ec543cc41de14912
Daniel Vincze <daniel.vincze@ibm.com>
2019-11-12 14:32:48 +0100

    feat: update dep tree when building is built

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Scripts to get the optimal travian kingdom build orders
 
 # TODO
 - [x] Factor in requirement cost when building has some
+- [x] When a building level is built, missing requirement costs should be updated on buildings depending on the built one
 - [ ] Factor in building time (with main building level)
 - [ ] Create ui instead of text file
 - [ ] Parse current village and suggest next buildings
--- scripts/idealBuildOrder.js
+++ scripts/idealBuildOrder.js
@@ -9,6 +9,12 @@ const capitalBuildings = ['palace', 'stonemason'];
 const built = [];
 const buildOrder = [];
 
+function debugWait(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+}
+
 function getCode(building, offset = 0) {
   return `${building.name}:${building.level - offset}`;
 }
@@ -43,20 +49,64 @@ function findMissingRequirements(building) {
   return uniqueBuildings(missingBuildings);
 }
 
-function forceBuild(building) {
+function updateDependencies(building) {
+  costData.map(buildingData => {
+    if (!buildingData.requirementsAdded) {
+      return;
+    }
+    const depIndex = buildingData.missing.findIndex(bd => {
+      return bd.name === building.name && bd.level === building.level;
+    });
+    if (depIndex === -1) {
+      return;
+    }
+    buildingData.missing.splice(depIndex, 1);
+    const requirementCostSum = buildingData.missing.reduce((sum, required) => sum + required.levelCost, 0);
+    const requirementCPSum = buildingData.missing.reduce((sum, required) => sum + required.cpGain, 0);
+    buildingData.levelCostWithRequirements = buildingData.levelCost + requirementCostSum;
+    buildingData.cpGainWithRequirements = buildingData.cpGain + requirementCPSum;
+    buildingData.costPerCpGainWithRequirements = buildingData.levelCostWithRequirements / buildingData.cpGainWithRequirements;
+    buildingData.requirementCount = buildingData.missing.length;
+  });
+  costData.sort((a, b) => {
+    return (a.costPerCpGainWithRequirements || a.costPerCpGain) - (b.costPerCpGainWithRequirements ||b.costPerCpGain);
+  });
+}
+
+function forceBuild(building, target) {
   // building is already up as a requirement
   if (built.includes(getCode(building))) {
     return;
   }
+  // target is not defined at root building, but defined later when force building required levels
+  if (!target) {
+    newTarget = getCode(building);
+  }
   const missing = findMissingRequirements(building);
 
   if (missing.length === 0) {
+    if (target && target !== getCode(building)) {
+      building.target = target;
+    }
+    updateDependencies(building);
     buildOrder.push(building);
     built.push(getCode(building));
   } else {
-    missing.map(forceBuild);
-    forceBuild(building);
+    missing.forEach(missingBuilding => forceBuild(missingBuilding, target || newTarget));
+    forceBuild(building, target || newTarget);
   }
+}
+
+function clearBuilding(building) {
+  // the building was previously listed as first, but with requirements, it was not good enough
+  // after the new sort, the building was not first anymore, now we got to it first again, but there
+  // are no longer missing levels
+  delete building.levelCostWithRequirements;
+  delete building.cpGainWithRequirements;
+  delete building.costPerCpGainWithRequirements;
+  delete building.requirementCount;
+  delete building.missing;
+  delete building.requirementsAdded;
 }
 
 function buildBuilding(building, isCapital) {
@@ -71,18 +121,19 @@ function buildBuilding(building, isCapital) {
   const missing = findMissingRequirements(building);
 
   if (missing.length === 0) {
+    clearBuilding(building);
+    updateDependencies(building);
     buildOrder.push(building);
     built.push(getCode(building));
   } else {
-    if (!building.requirementsAdded) {
-      const requirementCostSum = missing.reduce((sum, required) => sum + required.levelCost, 0);
-      const requirementCPSum = missing.reduce((sum, required) => sum + required.cpGain, 0);
-      building.levelCostWithRequirements = building.levelCost + requirementCostSum;
-      building.cpGainWithRequirements = building.cpGain + requirementCPSum;
-      building.costPerCpGainWithRequirements = building.levelCostWithRequirements / building.cpGainWithRequirements;
-      building.requirementCount = missing.length;
-      building.requirementsAdded = true;
-    }
+    const requirementCostSum = missing.reduce((sum, required) => sum + required.levelCost, 0);
+    const requirementCPSum = missing.reduce((sum, required) => sum + required.cpGain, 0);
+    building.levelCostWithRequirements = building.levelCost + requirementCostSum;
+    building.cpGainWithRequirements = building.cpGain + requirementCPSum;
+    building.costPerCpGainWithRequirements = building.levelCostWithRequirements / building.cpGainWithRequirements;
+    building.requirementCount = missing.length;
+    building.missing = missing;
+    building.requirementsAdded = true;
 
     if (building.costPerCpGainWithRequirements < (costData[0].costPerCpGainWithRequirements || costData[0].costPerCpGain)) {
       forceBuild(building);
@@ -98,8 +149,12 @@ function buildBuilding(building, isCapital) {
 function saveBuildOrder() {
   const buildOrderPretty = buildOrder.map(b => {
     let string = `${b.name} lvl${b.level}: You gain ${b.cpGain}cp for ${b.levelCost} res. Res/cp: ${b.costPerCpGain}\n`;
-    if (b.requirementsAdded) {
-      string += `It has ${b.requirementCount} required building levels (listed above).\nCombined cost: ${b.levelCostWithRequirements} adding ${b.cpGainWithRequirements} cp overall (avg: ${b.costPerCpGainWithRequirements})\n`;
+    if (b.target) {
+      string += `Required for ${b.target}\n`;
+    }
+    if (b.requirementsAdded && b.requirementCount > 0) {
+      string += `It has ${b.requirementCount} required building levels (listed above) ${b.missing.map(m => getCode(m)).join(', ')}.
+Combined cost: ${b.levelCostWithRequirements} adding ${b.cpGainWithRequirements} cp overall (avg: ${b.costPerCpGainWithRequirements})\n`;
     }
     return string;
   });
@@ -107,7 +162,7 @@ function saveBuildOrder() {
   fs.writeFileSync('buildOrder.json', JSON.stringify(buildOrder, null, 2));
 }
 
-async function computeIdealBuildOrder(isCapital) {
+function computeIdealBuildOrder(isCapital) {
   while (costData.length > 0) {
     const nextBestBuilding = costData.shift();
     buildBuilding(nextBestBuilding, isCapital);
