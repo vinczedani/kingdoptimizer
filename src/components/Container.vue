531b354755dcecaa5cfa2f352e1811fb02b5c945
Daniel Vincze <daniel.vincze@ibm.com>
2019-11-13 15:42:16 +0100

    update instructions

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
     <div class="usage">
-      <div style="width: 20%;">You can add multiple villages, they will be stored in your browser, so whenever you come back, they will be there. You can rename the active village</div>
-      <div style="width: 30%;">Set current building levels to get up to date recommendations. Click the "Buildings" subititle to sort buildings alphabetically</div>
-      <div style="width: 50%;">The recommendations are sorted based on res/cp, the first is the cheapest. Click the recommendation to build it in your active village</div>
+      <div style="width: 20%;">
+        You can add multiple villages, they will be stored in your browser, so whenever you come back, they will be there.<br>
+        You can rename the active village</div>
+      <div style="width: 30%;">
+        Set current building levels to get up to date recommendations.<br>
+        Click the "Buildings" subititle to sort buildings alphabetically</div>
+      <div style="width: 50%;">
+        The recommendations are sorted based on res/cp, the first is the cheapest.<br>
+        Sometimes, it's worth it to build a building with worse res/cp to be able to build a better res/cp building. When calculating res/cp on buildings with missing requirements, the app uses averages.<br>
+        Click the recommendation to build it in your active village</div>
     </div>
     <div class="container">
       <Villages/>
